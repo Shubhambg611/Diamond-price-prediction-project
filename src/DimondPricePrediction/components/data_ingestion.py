f7644bf474a66d7769a698bf63d0eef9677c21b7
Shubhambg611 <s.bgadhave611@gmail.com>
2024-01-27 18:38:32 +0530

    data

--- src/DimondPricePrediction/components/data_ingestion.py
+++ src/DimondPricePrediction/components/data_ingestion.py
@@ -1,53 +1,56 @@
 import pandas as pd
 import numpy as np
 from src.DimondPricePrediction.logger import logging
-from src.DimondPricePrediction.exception import customexception 
+from src.DimondPricePrediction.exception import customexception
 
 import os
 import sys
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-from pathlib import Path 
+from pathlib import Path
 
 class DataIngestionConfig:
-    raw_data_path:str=os.path.join("artifact","raw.csv")
-    train_data_path:str=os.path.join("artifact","train.csv")
-    test_data_path:str=os.path.join("artifact","test.csv")
+    raw_data_path:str=os.path.join("artifacts","raw.csv")
+    train_data_path:str=os.path.join("artifacts","train.csv")
+    test_data_path:str=os.path.join("artifacts","test.csv")
+
 
 class DataIngestion:
     def __init__(self):
-        self.injection_config=DataIngestionConfig()
+        self.ingestion_config=DataIngestionConfig()
+        
     
-    def initiate_data_injection(self):
-        logging.info("Data injection started")
+    def initiate_data_ingestion(self):
+        logging.info("data ingestion started")
         
         try:
-            data = pd.read_csv(Path(os.path.join("notebooks/data","train.csv")))
-            logging.info("read the data set as data frame ")
-            
-            os.makedirs(os.path.dirname(os.path.join(self.injection_config.raw_data_path)),exist_ok=True)
-            data.to_csv(self.injection_config.raw_data_path,index=False)
-            logging.info("I have saved the raw data in artifact folder")
+            data=pd.read_csv(Path(os.path.join("D:/Diamond price prediction project/notebooks/data","train.csv")))
+            logging.info(" i have read dataset as a df")
             
             
-            logging.info("I have now performed train test split")
+            os.makedirs(os.path.dirname(os.path.join(self.ingestion_config.raw_data_path)),exist_ok=True)
+            data.to_csv(self.ingestion_config.raw_data_path,index=False)
+            logging.info(" i have saved the raw dataset in artifact folder")
             
-            train_data,test_data = train_test_split(data,test_size=0.25) 
-            logging.info("train test split completed")  
+            logging.info("here i have performed train test split")
             
-            train_data.to_csv(self.injection_config.train_data_path,index=False)
-            test_data.to_csv(self.injection_config.test_data_path,index=False)
+            train_data,test_data=train_test_split(data,test_size=0.25)
+            logging.info("train test split completed")
             
-            logging.info("Data ingesion part is completed")
+            train_data.to_csv(self.ingestion_config.train_data_path,index=False)
+            test_data.to_csv(self.ingestion_config.test_data_path,index=False)
+            
+            logging.info("data ingestion part completed")
             
             return (
+                 
                 
-                self.injection_config.train_data_path,
-                self.injection_config.test_data_path
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
             )
             
+            
         except Exception as e:
-            logging.info("excpetion during occured ")
-            raise customexception(e,sys)
-        
-        
+           logging.info("exception during occured at data ingestion stage")
+           raise customexception(e,sys)
+    
